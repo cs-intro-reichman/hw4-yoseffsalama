2b4e9d2ed289be7e2123df4df113df2fd648838d
yoseffsalama <yosef.salamaruah@post.runi.ac.il>
2024-12-06 00:10:49 +0200

    Update KeywordsDetector.java

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -13,14 +13,20 @@ public class KeywordsDetector {
             "Our new technology presents a significant paradigm shift",
             "Effective presentations must be clear, concise, and humble"
         };
-        // Some keywords that typically signal bullshit contents in business presentations 
+
         String[] keywords = {"synergy", "disrupt", "leverage", "Paradigm", "transform"};
         detectAndPrint(sentences, keywords);
+       
     }
 
-    // Iterates through all the sentences.
-    // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+    for (int i = 0; i < sentences.length; i++) {
+        for (int j = 0; j < keywords.length; j++) {
+        if (MyString.contains(MyString.lowerCase(sentences[i]), MyString.lowerCase(keywords[j]))) {
+        System.out.println(sentences[i]);
+        break;
+                }
+            }
+        }
     }
 }
