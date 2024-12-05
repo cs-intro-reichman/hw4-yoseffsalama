d1bc31a6762ef16949e7b2f07b301d425a17c407
yoseffsalama <yosef.salamaruah@post.runi.ac.il>
2024-12-06 00:13:44 +0200

    Update MyString.java

--- MyString.java
+++ MyString.java
@@ -7,24 +7,50 @@ public class MyString {
         System.out.println("lowercase : " + lowerCase("lowercase"));
 
         System.out.println("Testing contains:");
-        System.out.println(contains("unhappy", "happy")); // true
-        System.out.println(contains("happy", "unhappy")); // false
-        System.out.println(contains("historical", "story")); // false
-        System.out.println(contains("psychology", "psycho")); // true
-        System.out.println(contains("personality", "son")); // true
-        System.out.println(contains("personality", "dad")); // false
-        System.out.println(contains("resignation", "sign")); // true
+        System.out.println(contains("unhappy", "happy")); 
+        System.out.println(contains("happy", "unhappy")); 
+        System.out.println(contains("historical", "story")); 
+        System.out.println(contains("psychology", "psycho")); 
+        System.out.println(contains("personality", "son")); 
+        System.out.println(contains("personality", "dad")); 
+        System.out.println(contains("resignation", "sign")); 
     }
 
-    /** Returns the lowercase version of the given string. */
-    public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+
+    public static String lowerCase(String str) { 
+        String result = "";
+        for (int i=0; i<str.length(); i++) {
+        char c= str.charAt(i);
+        if (c>='A' && c<='Z') {
+          c+=('a'-'A');
+        }
+        result+=c;   
+      }
+      return  result;
     }
 
-    /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        int m=0;
+        if (str1.length()<str2.length()) {
+            return false;
+        }
+        for (int i=0; i<str1.length(); i++) {
+            m=0;
+            for (int j=0; j<str2.length(); j++) {
+                if (str1.charAt(i)!=str2.charAt(j)) {
+                    break;
+                }
+                else {
+             i++; 
+             m++;
+                }
+            }
+            if (m==str2.length()) {
+                return true;
+            } 
+        }
+       
         return false;
+        
     }
 }
